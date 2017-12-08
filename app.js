0eae3d5a1d8aea340c57f099735854eec5be3a4d
Enes Malik Sayan <enesmaliksayan@gmail.com>
2017-12-08 23:24:26 +0300

    fix

--- app.js
+++ app.js
@@ -44,15 +44,12 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use(function(req, res, next) {
+  if( req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url)
+  } else {
+    next();
+  }
 });
 
 app.listen(3000, () => {
